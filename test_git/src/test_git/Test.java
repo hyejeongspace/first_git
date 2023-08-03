3e518ad01082cf31d3f5998cb774282dd5d04eb7
hyejeongspace <hjeong91108@gmail.com>
2023-08-03 15:59:01 +0900

    branch-2

---
+++ test_git/src/test_git/Test.java
@@ -0,0 +1,13 @@
+package test_git;
+
+public class Test {
+	public void method1() {
+		System.out.println("브랜치 1에서 수정할 부분");
+	}
+	public void method2() {
+		System.out.println("브랜치 2에서 수정");
+	}
+	public void method3() {
+		System.out.println("유지 파트");
+	}
+}
