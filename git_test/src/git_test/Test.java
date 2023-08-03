6550a2c33dbe3045058bdffc73826d66a8c5ec4d
hyejeongspace <hjeong91108@gmail.com>
2023-08-03 15:42:45 +0900

    branch1

--- git_test/src/git_test/Test.java
+++ git_test/src/git_test/Test.java
@@ -2,7 +2,7 @@ package git_test;
 
 public class Test {
 	public void method1() {
-		System.out.println("브랜치 1에서 수정할 부분");
+		System.out.println("브랜치 1에서 수정");
 	}
 	public void method2() {
 		System.out.println("브랜치 2에서 수정할 부분");
